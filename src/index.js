5dfc2d1273cf3aecb0ca24c74b0fb238be390a8f
Sudipta Mishra <asksudipta@gmail.com>
2022-09-19 10:04:33 +0200

    added bootstrap and css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //import react-dom
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
 
 
 const message=<h1> React Workshop1 Project</h1>
-reactDOM.render( message, document.getElementById('root') );
+ReactDOM.render( message, document.getElementById('root') );
